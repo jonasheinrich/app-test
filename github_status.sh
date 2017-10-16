4b4d71a5a2e7fcd5e7a09a8f6160f356dbdc1742
Jonas Heinrich <jonas.heinrich@akelius.de>
2017-10-16 13:15:25 +0200

    Testing setup

--- github_status.sh
+++ github_status.sh
@@ -4,5 +4,5 @@ curl -u jonasheinrich:$GITHUB_ACCESS_TOKEN \
   -i \
   -H "Content-Type: application/json" \
   -X POST \
-  -d "{\"state\": \"$1\", \"context\": \"ACM UI Tests\"}" \
+  -d "{\"state\": \"$1\", \"context\": \"ACM UI Tests\", \"target_url\": \"https://github.com/jonasheinrich/app-test/pull/$TRAVIS_PULL_REQUEST\"}" \
   https://api.github.com/repos/jonasheinrich/fe-test/statuses/$FE_COMMIT_HASH
---
+++ test_runner.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# trigger ui tests here
+exit 0
